eb5f4dbcb70b0c82af3afcb35680390348fab191
Pawel Osmolski <pawel@pawel-osmolski.com>
2025-06-25 11:10:59 +0100

    Footer now displays links in an unordered list

--- partials/footer.php
+++ partials/footer.php
@@ -1,6 +1,12 @@
 <footer role="contentinfo">
 	<div class="footer-wrapper">
-		<div class="footer-links"><a href="#" id="toggle-index">Folders</a> | <a href="#" id="toggle-settings">Settings</a> | <a href="/phpmyadmin/">PHPMyAdmin</a></div>
+		<ul class="footer-links">
+			<li><a href="#" id="toggle-index">Folders</a></li>
+			<li><a href="#" id="toggle-settings">Settings</a></li>
+			<li><a href="/phpmyadmin/" target="_blank">PHPMyAdmin</a></li>
+		</ul>
 	</div>
-	<div class="footer-text"><p><span class="quote">“</span>It’s not a bug. It’s an undocumented feature!<span class="quote">”</span></p></div>
+	<div class="footer-text">
+		<p><span class="quote">“</span>It’s not a bug. It’s an undocumented feature!<span class="quote">”</span></p>
+	</div>
 </footer>
